3017afe29bfa57eeca782e3bcef122c63ac812ff
Faruq Rasid <faruq.mohamed@thoughtworks.com>
2020-11-01 18:33:53 +0800

    Implement parsing of line item string to Item

--- src/main/java/sg/ruqqq/salestax/Item.java
+++ src/main/java/sg/ruqqq/salestax/Item.java
@@ -2,17 +2,31 @@ package sg.ruqqq.salestax;
 
 import lombok.Getter;
 
+import java.util.Arrays;
+
 @Getter
 public class Item {
 
+    private final int qty;
     private final String name;
     private final boolean imported;
     private final int price;
 
-    public Item(String name, int price, boolean imported) {
+    public Item(int qty, String name, int price, boolean imported) {
+        this.qty = qty;
         this.name = name;
         this.imported = imported;
         this.price = price;
+    }
+
+    public Item(String lineItem) {
+        String[] splitLine = lineItem.split(" ");
+        String qtyString = splitLine[0];
+        String priceString = splitLine[splitLine.length - 1];
+        this.qty = Integer.parseInt(qtyString);
+        this.name = String.join(" ", Arrays.asList(splitLine).subList(1, splitLine.length - 1));
+        this.imported = name.startsWith("imported");
+        this.price = (int) (Double.parseDouble(priceString) * 100);
     }
 
     public int getTax() {
--- src/test/java/sg/ruqqq/salestax/ItemTest.java
+++ src/test/java/sg/ruqqq/salestax/ItemTest.java
@@ -8,37 +8,45 @@ class ItemTest {
 
     @Test
     void item_is_an_imported_product() {
-        Item item = new Item("imported box of chocolate", 1500, true);
+        Item item = new Item(1, "imported box of chocolate", 1500, true);
         assertTrue(item.isImported());
     }
 
     @Test
     void item_is_not_an_imported_product() {
-        Item item = new Item("box of chocolate", 1500, false);
+        Item item = new Item(1, "box of chocolate", 1500, false);
         assertFalse(item.isImported());
     }
 
     @Test
     void calculate_tax_of_local_product() {
-        Item item = new Item("box of chocolate", 1500, false);
+        Item item = new Item(1, "box of chocolate", 1500, false);
         assertEquals(150, item.getTax());
     }
 
     @Test
     void calculate_tax_of_imported_product() {
-        Item item = new Item("imported box of chocolate", 1000, true);
+        Item item = new Item(1, "imported box of chocolate", 1000, true);
         assertEquals(155, item.getTax());
     }
 
     @Test
     void calculate_tax_of_imported_product_with_rounding_up() {
-        Item item = new Item("imported box of chocolate", 1500, true);
+        Item item = new Item(1, "imported box of chocolate", 1500, true);
         assertEquals(235, item.getTax());
     }
 
     @Test
     void calculate_tax_of_imported_product_with_rounding_up_to_next_10_cents() {
-        Item item = new Item("imported box of chocolate", 1530, true);
+        Item item = new Item(1, "imported box of chocolate", 1530, true);
         assertEquals(240, item.getTax());
+    }
+
+    @Test
+    void parse_string_to_item() {
+        Item item = new Item("1 imported box of chocolates 10.00");
+        assertEquals(1, item.getQty());
+        assertEquals("imported box of chocolates", item.getName());
+        assertEquals(1000, item.getPrice());
     }
 }
