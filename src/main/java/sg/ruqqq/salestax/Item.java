b0e36c65e067155f7d66338dced6806bf938b37a
Faruq Rasid <faruq.mohamed@thoughtworks.com>
2020-11-01 17:23:20 +0800

    Make the test pass

--- src/main/java/sg/ruqqq/salestax/Item.java
+++ src/main/java/sg/ruqqq/salestax/Item.java
@@ -1,4 +1,15 @@
 package sg.ruqqq.salestax;
 
+import lombok.Getter;
+
+@Getter
 public class Item {
+
+    private final String name;
+    private final boolean imported;
+
+    public Item(String name) {
+        this.name = name;
+        this.imported = name.startsWith("imported");
+    }
 }
