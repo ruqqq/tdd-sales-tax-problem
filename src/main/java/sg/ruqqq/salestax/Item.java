8d772c9a856beca133f6245eea5088cc20e43990
Faruq Rasid <faruq.mohamed@thoughtworks.com>
2020-11-01 17:28:19 +0800

    Implement to pass the test

--- src/main/java/sg/ruqqq/salestax/Item.java
+++ src/main/java/sg/ruqqq/salestax/Item.java
@@ -7,9 +7,15 @@ public class Item {
 
     private final String name;
     private final boolean imported;
+    private final double price;
 
     public Item(String name, double price) {
         this.name = name;
         this.imported = name.startsWith("imported");
+        this.price = price;
+    }
+
+    public double getTax() {
+        return price * 0.1;
     }
 }
